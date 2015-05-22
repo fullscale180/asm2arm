1ae08026ced305b740172468bd8cf15f814e3de7
Ercenk Keresteci <ercenk@ercenk.com>
2015-05-21 17:49:56 -0700

    first template deployment

--- asm2arm.psm1
+++ asm2arm.psm1
@@ -1,3 +1,7 @@
+Set-StrictMode -Version 3
+
+$apiVersion = "2015-05-01-preview"
+
 <#
 .Synopsis
    Discover a single VM deployment on the Azure Service Management (ASM) deployments and
@@ -51,14 +55,7 @@ function Add-AzureSMVmToRM
         [ValidateNotNull()]
         [ValidateNotNullOrEmpty()]
         [PersistentVm]
-        $VM,
-
-        # Name of the VM
-        [Parameter(Mandatory=$true)]
-        [string]
-        [ValidateNotNull()]
-        [ValidateNotNullOrEmpty()]
-        $TargetStorageAccountName
+        $VM
     )
 
     if ($psCmdlet.ParameterSetName -eq "Service and VM Name")
@@ -95,10 +92,207 @@ function Add-AzureSMVmToRM
     }
 
     $cloudService = Azure\Get-AzureService -ServiceName $VM.ServiceName
-    $vmLocation= $cloudService.Location
+    $location= $cloudService.Location
 
-    $armImageReference = Azure\Get-AzureArmImageRef -Location $vm.$vmLocation -Image $vmImage
+    $armImageReference = Azure\Get-AzureArmImageRef -Location $location -Image $vmImage
 
+    $currentSubscription = AzureResourceManager\Get-AzureSubscription -Current
+    $canonicalSubscriptionName = ($($currentSubscription.SubscriptionName -replace [regex]'[aeiouAEIOU]','') -replace [regex]'[^a-zA-Z]','').ToLower()
+
+    $resources = @()
+
+    $parametersObject = @{}
+    $parametersObject.Add('location', $(New-ArmTemplateParameter -Type "string" -Description "location where the resources are going to be deployed to" `
+                                            -AllowedValues @("East US", "West US", "West Europe", "East Asia", "South East Asia"))) 
+
+    $parameters = New-Object -TypeName PSCustomObject $parametersObject
+
+    $storageAccountName = Get-StorageAccountName -NamePrefix $canonicalSubscriptionName
+    if (-not $(Azure\Test-AzureName -Storage $storageAccountName))
+    {
+        $storageAccountResource = New-StorageAccountResource -Name $storageAccountName -Location '[parameters(''location'')]'
+        $resources += $storageAccountResource
+    }
+    
+    
+    $template = New-ArmTemplate -Parameters $parameters -Resources $resources
+    $templateFileName =  [IO.Path]::GetTempFileName()
+    $template | Out-File $templateFileName
+
+    AzureResourceManager\New-AzureResourceGroup -Name ecarm  -TemplateFile $templateFileName
+}
+
+
+function New-ArmTemplateParameter
+{
+    Param
+    (
+        [ValidateSet("string", "securestring", "int", "bool", "array", "object")]
+        $Type,
+        $Description,
+        [array]
+        $AllowedValues,
+        $DefaultValue
+    )
+
+    $parameterDefinition = @{'type' = $Type; 'metadata' = New-Object -TypeName PSCustomObject @{'description' = $Description}}
+
+    if ($AllowedValues)
+    {
+        $parameterDefinition.Add('allowedValues', $AllowedValues)
+    }
+
+    if ($DefaultValue)
+    {
+        $parameterDefinition.Add('defaultValue', $DefaultValue)
+    }
+
+    return New-Object -TypeName PSCustomObject $parameterDefinition
+  
+}
+
+
+function Get-StorageAccountName
+{
+    [OutputType([string])]
+    Param
+    (
+        $NamePrefix
+    )
+
+    # Decide if we need to create a storage account
+    $storageAccountName = $NamePrefix.Substring(0,20) + 'arm'
+
+    $storageAccountExists = $false
+
+    $retryStorageAccountName = $true
+    $index = 0
+
+    do
+    {
+        $storageAccountExists = Azure\Test-AzureName -Storage $storageAccountName
+
+        if ($storageAccountExists)
+        {
+            # Get-AzureStorageAccount -Name <name> always returns all of the V2 storage accounts on the subscription
+            # at this time. Use a workaround to bring the accounts to an array and check instead.
+            $storageAccounts = AzureResourceManager\Get-AzureStorageAccount | Select-Object Name
+        
+            if ($storageAccounts.Contains($storageAccountName))
+            {
+                # If we are here, this is a storage account on this subscription, and using V2
+                $retryStorageAccountName = $false
+            } 
+            else
+            {
+                # If we are here, that means, storage account exists but some other subscription has it
+                $retryStorageAccountName = $true
+                $storageAccountName = "{0:00}{1}arm" -f $NamePrefix.Substring(0,18), $index++
+            }
+        }
+        else
+        {
+            $retryStorageAccountName = $false
+        }
+    }
+    until (-not $retryStorageAccountName)
+
+    return $storageAccountName
+}
+
+
+function New-ResourceTemplate 
+{
+    Param
+    (
+        $Type,
+        $Name,
+        $ApiVersion,
+        $Location,
+        $Properties,
+        [array]
+        $DependsOn,
+        [PSCustomObject]
+        $Resources
+    )
+
+    $template = @{
+        "name" = $Name;
+        "type"=  $Type;
+        "apiVersion" = $ApiVersion;
+        "location" = $Location;
+        "tags" = New-Object -TypeName PSCustomObject @{"deploymentReason" = "ARM";};
+        "properties" = $Properties;
+    }
+
+    if ($Resources) 
+    {
+        $template.Add("resources", $Resources)
+    }
+
+    if ($DependsOn)
+    {
+        $template.Add("dependsOn", $DependsOn)
+    }
+
+    return New-Object -TypeName PSCustomObject $template
+}
+
+function New-ArmTemplate 
+{
+    Param
+    (
+        $Version,
+        [PSCustomObject]
+        $Parameters,
+        [PSCustomObject]
+        $Variables,
+        [PSCustomObject[]]
+        $Resources,
+        [PSCustomObject[]]
+        $Outputs
+    )
+
+    $template = @{
+        '$schema' = 'https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#';
+        'parameters' = $Parameters;
+        'resources' = $Resources;
+    }
+
+    if (-not $Version)
+    {
+        $Version = '1.0.0.0'
+    }
+
+    $template.Add('contentVersion', $Version)
+
+    if ($Outputs) 
+    {
+        $template.Add("outputs", $Outputs)
+    }
+
+    if ($Variables)
+    {
+       $template.Add("variables", $Variables)
+    }
+
+    $templateObject = New-Object -TypeName PSCustomObject $template
+    return ConvertTo-Json $templateObject -Depth 5
+}
+
+function New-StorageAccountResource
+{
+    Param
+    (
+        $Name,
+        $Location
+    )
+
+    $createProperties = New-Object -TypeName PSCustomObject @{"accountType" = "Standard_LRS";}
+    $resource = New-ResourceTemplate -Type "Microsoft.Storage/storageAccounts" -Name $Name `
+        -Location $Location -ApiVersion "2015-05-01-preview" -Properties $createProperties
+
+    return $resource
 }
 
 <#
@@ -107,7 +301,7 @@ function Add-AzureSMVmToRM
 .DESCRIPTION
    Do a search on the ARM image catalog, based on the input ASM VM Image.
 .EXAMPLE
-   Get-AzureArmImageRef -Location $vm.$vmLocation -Image $vmImage
+   Get-AzureArmImageRef -Location $vm.$location -Image $vmImage
 #>
 function Get-AzureArmImageRef
 {
@@ -213,7 +407,7 @@ function Get-AzureArmImageRef
     if ($images.Length -gt 0)
     {
         Write-Output "Found the following potential images:"
-        $images | Select Option, Publisher, Offer, Skus, Version | Format-Table -AutoSize 
+        $images | Select-Object Option, Publisher, Offer, Skus, Version | Format-Table -AutoSize 
         $option = Read-Host -Prompt "Please type in the Option number and press Enter"
 
         return $images[$option].Id
@@ -224,5 +418,3 @@ function Get-AzureArmImageRef
         return @()
     }
 }
-
-
