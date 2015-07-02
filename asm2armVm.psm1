ba3e321c1ee42ed09178839198e60ad763980865
Ercenk Keresteci <ercenk@ercenk.com>
2015-07-02 10:36:09 -0700

    addvm

---
+++ asm2armVm.psm1
@@ -0,0 +1,25 @@
+﻿function New-VmStorageProfile 
+{
+    Param (
+
+        [PsCustomObject]
+        $ArmImageReference,
+
+        [Microsoft.WindowsAzure.Commands.ServiceManagement.Model.PersistentVMRoleContext]
+        $VM,
+
+        [switch]
+        $KeepDisks,
+
+        [string]
+        $StorageAccountName,
+
+        [switch]
+        $CopyDisks
+    )
+
+    if ($ArmImageReference)
+    {
+        $imageReference = new-
+    }
+}
