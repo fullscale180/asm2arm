d84c2ad0535f3cd559b4e729480cfad85d5d0fe9
Ercenk Keresteci <ercenk@ercenk.com>
2015-07-06 20:20:09 +0000

    move to desktop

--- asm2armVm.psm1
+++ asm2armVm.psm1
@@ -18,8 +18,33 @@
         $CopyDisks
     )
 
+    $storageProfile = @{}
     if ($ArmImageReference)
     {
-        $imageReference = new-
+        $imageReference =[PSCustomObject] @{'publisher' = $ArmImageReference.Publisher; `
+                                            'offer'= $ArmImageReference.Offer;
+                                            'sku'= $ArmImageReference.Skus;
+                                            'version'= $ArmImageReference.Version;}  
+        $storageProfile.Add('imageReference', $imageReference)                  
     }
+
+    $osDisk = @{}
+    $dataDisks = @()
+
+    if ($CopyDisks.IsPresent)
+    {
+        $url = $VM.VM.OSVirtualHardDisk.MediaLink
+
+        # Get the source storage account name from the URL the format on the 
+        # blob store should be <storage account name>..blob.core.windows.net
+        $sourceStorageAccountName = ([System.Uri]$url).Host.Split('.')[0]
+
+        $sourceAccountKey = (Azure\Get-AzureStorageKey -StorageAccountName $sourceStorageAccountName).Primary
+        $sourceContext = Azure\New-AzureStorageContext -StorageAccountName $sourceStorageAccountName -StorageAccountKey $sourceAccountKey
+
+
+
+
+    }
+
 }
