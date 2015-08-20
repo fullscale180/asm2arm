445dee82a692715cabcb98f559737b26e5aa71c6
Ercenk Keresteci <ercenk@ercenk.com>
2015-08-20 09:59:03 -0700

    added legal disclamer

--- asm2arm/asm2arm.psm1
+++ asm2arm/asm2arm.psm1
@@ -1,4 +1,15 @@
 <#
+    © 2015 Microsoft Corporation. All rights reserved. This sample code is not supported under any Microsoft standard support program or service. 
+    This sample code is provided AS IS without warranty of any kind. Microsoft disclaims all implied warranties including, without limitation, 
+    any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance 
+    of the sample code and documentation remains with you. In no event shall Microsoft, its authors, or anyone else involved in the creation, 
+    production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business 
+    profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the 
+    sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
+#>
+
+
+<#
 .Synopsis
    Discover a single VM deployment on the Azure Service Management (ASM) deployments and generate templates and scripts
    then optionally deploy a VM with the same image to an Azure Resource Manager (ARM) deployment.
--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,1 +1,13 @@
+@ECHO OFF
+GOTO Start
+ © 2015 Microsoft Corporation. All rights reserved. This sample code is not supported under any Microsoft standard support program or service. 
+ This sample code is provided AS IS without warranty of any kind. Microsoft disclaims all implied warranties including, without limitation, 
+ any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance 
+ of the sample code and documentation remains with you. In no event shall Microsoft, its authors, or anyone else involved in the creation, 
+ production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business 
+ profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the 
+ sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
+
+
+:Start
 %SystemRoot%\SysWOW64\WindowsPowerShell\v1.0\powershell.exe -NoExit -ExecutionPolicy Bypass -File "bootstrap.ps1"
